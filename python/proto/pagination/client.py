6f05cc3e3ea0dac53ee612f188728c9f60b760de
griddic <griddic@mail.ru>
2021-09-21 22:41:42 +0300

    pager

--- python/proto/pagination/client.py
+++ python/proto/pagination/client.py
@@ -1,21 +1,24 @@
 import grpc
 
-import hello_pb2_grpc
-import hello_pb2
+import gen.pb_python.hello_pb2_grpc as hello_pb2_grpc
+import gen.pb_python.hello_pb2 as hello_pb2
 
 import grpc._auth
 
 
 def main():
-    channel = grpc.insecure_channel('localhost:50051', grpc.access_token_call_credentials("kokoko"))
-    # channel = grpc.secure_channel('localhost:50051', grpc.access_token_call_credentials("kokoko"))
-    channel = grpc.secure_channel('localhost:50051',
-                                  grpc.composite_channel_credentials(grpc.local_channel_credentials(),
-                                                                     grpc.access_token_call_credentials("kokoko")))
+    channel = grpc.insecure_channel('localhost:50051')
     stub = hello_pb2_grpc.UpperStub(channel)
 
-    print(stub.Up(hello_pb2.Player(name="пЕтька"), metadata=(("authorizationnnn", "Bearer bebearer"),) ).name)
-    stub.Up(hello_pb2.Player(name="пЕтька")).Unpack()
+    next_page_token = ''
+    while True:
+        res = stub.ListThousand(hello_pb2.ListRequests(page_size=17,
+                                                       page_token=next_page_token))
+        print(res.nums)
+        print(res.next_page_token)
+        next_page_token = res.next_page_token
+        if not next_page_token:
+            break
 
 
 if __name__ == '__main__':
--- python/proto/pagination/server.py
+++ python/proto/pagination/server.py
@@ -1,25 +1,110 @@
+import base64
+import hashlib
+import json
 import logging
 import concurrent.futures as futures
 
-
-# import gen.pb_python.hello_pb2_grpc as hello_pb2_grpc
-# import gen.pb_python.hello_pb2 as hello_pb2
-import hello_pb2_grpc
-import hello_pb2
-
+import gen.pb_python.hello_pb2_grpc as hello_pb2_grpc
+import gen.pb_python.hello_pb2 as hello_pb2
 
 import grpc
-import grpc_interceptor
-from grpc_interceptor import exceptions
-from grpc import RpcError
+
+
+class Crypter:
+    def encrypt(self, string) -> str:
+        return string
+
+    def decrypt(self, string) -> str:
+        return string
+
+
+class Pager:
+    def __init__(self, crypter):
+        self.crypter = crypter
+
+    def next_page_token(self, offset, filters_hash):
+        raw = json.dumps((offset, filters_hash))
+        coded: str = self.crypter.encrypt(raw)
+        encoded = base64.encodebytes(coded.encode('utf-8'))
+        return encoded.decode('utf-8')
+
+    def parse_next_page_token(self, next_page_token: str):
+        decoded = base64.decodebytes(next_page_token.encode('utf-8'))
+        decrypted = self.crypter.decrypt(decoded.decode('utf-8'))
+        return json.loads(decrypted)
+
+
+class InvalidPageRequest(Exception):
+    pass
+
+
+class Pager1:
+    def __init__(self, page_token, page_size, filter, crypter):
+        self.crypter = crypter
+        self.filter_hash = get_hash(filter or '')
+        first_page = not page_token
+        if first_page:
+            self._offset = 0
+        else:
+            self._offset, filter_hash = self._parse_next_page_token(page_token)
+            if filter_hash != self.filter_hash:
+                raise InvalidPageRequest('following page request should be with the same filter')
+        self._page_size = page_size
+
+        self._shift = None
+
+    @property
+    def offset(self):
+        return self._offset
+
+    @property
+    def page_size(self):
+        return self._page_size
+
+    def _parse_next_page_token(self, next_page_token: str):
+        decoded = base64.decodebytes(next_page_token.encode('utf-8'))
+        decrypted = self.crypter.decrypt(decoded.decode('utf-8'))
+        return json.loads(decrypted)
+
+    def shift(self, shift):
+        self._shift = shift
+
+    @property
+    def next_page_token(self):
+        assert self._shift is not None
+        if self._shift == 0:
+            return ''
+        raw = json.dumps((self.offset + self._shift, self.filter_hash))
+        coded: str = self.crypter.encrypt(raw)
+        encoded = base64.encodebytes(coded.encode('utf-8'))
+        return encoded.decode('utf-8')
+
+
+
+def get_hash(string: str):
+    return hashlib.sha1(string.encode('utf-8')).hexdigest()
+
+
+# if __name__ == '__main__':
+#     pager = Pager(Crypter())
+#     args = (0, 'eee')
+#     token = pager.next_page_token(*args)
+#     print(token)
+#
+#     new_args = pager.parse_next_page_token(token)
+#     print(new_args)
 
 
 class Greeter(hello_pb2_grpc.UpperServicer):
+    def ListThousand(self, request: hello_pb2.ListRequests, context: grpc.ServicerContext):
+        pager = Pager1(request.page_token, request.page_size or 100, request.filters, Crypter())
 
-    def Up(self, request, context: grpc.ServicerContext):
-        # context.abort(grpc.StatusCode.UNAUTHENTICATED, "cococ")
-        # context.abort_with_status(grpc.StatusCode.CANCELLED)
-        return hello_pb2.Player(name=str(request.name).upper())
+        nums = list(range(pager.offset, min(pager.offset + pager.page_size, 1000)))
+        pager.shift(len(nums))
+
+        ans = hello_pb2.Numbers(nums=nums,
+                                next_page_token=pager.next_page_token)
+        return ans
 
 
 def serve():
@@ -29,7 +114,7 @@ def serve():
     server.add_insecure_port('[::]:50051')
 
     h = server._state.generic_handlers[0]
-    print("__>>>",h)
+    print("__>>>", h)
     server.start()
     server.wait_for_termination()
 
@@ -38,6 +123,7 @@ def main():
     logging.basicConfig()
     serve()
     grpc.RpcError()
+
 
 if __name__ == '__main__':
     main()
